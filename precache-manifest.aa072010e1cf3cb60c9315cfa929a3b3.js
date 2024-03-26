7db1f44aa9505f341f120a561d659fcadbd6d053
shrutitaylor <shruti128770@gmail.com>
2024-03-27 01:14:13 +1100

    Updates

---
+++ precache-manifest.aa072010e1cf3cb60c9315cfa929a3b3.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d4e2b02c0ee581df89239a43b31a40b",
+    "url": "/Shruti-Protfolio/index.html"
+  },
+  {
+    "revision": "711e0ab0e46e8fac9373",
+    "url": "/Shruti-Protfolio/static/css/main.96d2c5a8.chunk.css"
+  },
+  {
+    "revision": "43807829f6b11b5f6139",
+    "url": "/Shruti-Protfolio/static/js/2.150da6fa.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Shruti-Protfolio/static/js/2.150da6fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "711e0ab0e46e8fac9373",
+    "url": "/Shruti-Protfolio/static/js/main.d3cae5f0.chunk.js"
+  },
+  {
+    "revision": "f93b049e59e8290b66cc",
+    "url": "/Shruti-Protfolio/static/js/runtime-main.c741da65.js"
+  },
+  {
+    "revision": "bffe0c4aba5a01b7af575d41c14ec05b",
+    "url": "/Shruti-Protfolio/static/media/hiIamShruti.bffe0c4a.png"
+  },
+  {
+    "revision": "b810c0d32d375e0ac48a8c2bcb12b9b9",
+    "url": "/Shruti-Protfolio/static/media/main_project2.b810c0d3.JPG"
+  },
+  {
+    "revision": "551025531f81ad1847eb420f8ac4d2e0",
+    "url": "/Shruti-Protfolio/static/media/main_project3.55102553.png"
+  },
+  {
+    "revision": "e7706002f53cd2f029cf8dccd01fcda0",
+    "url": "/Shruti-Protfolio/static/media/main_project4.e7706002.JPG"
+  },
+  {
+    "revision": "4ae9df0b1aaabd25dbb852fe21e8536f",
+    "url": "/Shruti-Protfolio/static/media/main_project5.4ae9df0b.PNG"
+  },
+  {
+    "revision": "fd1b8d8c4a92c8f2742d395981456828",
+    "url": "/Shruti-Protfolio/static/media/shruti-logo-nobg.fd1b8d8c.png"
+  },
+  {
+    "revision": "a9202bf3a2ec21aed1b2ad18f63abfd8",
+    "url": "/Shruti-Protfolio/static/media/shrutidp.a9202bf3.jpg"
+  }
+]);
